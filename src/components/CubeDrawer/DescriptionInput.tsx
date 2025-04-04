8825c7d1c987369ee2c361235160cfaf0a1e9176
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-04-04 23:57:52 +0200

    feat: added animation on copy

--- src/components/CubeDrawer/DescriptionInput.tsx
+++ src/components/CubeDrawer/DescriptionInput.tsx
@@ -5,11 +5,14 @@ import clsx from 'clsx';
 export default function DescriptionInput({value, onChange}) : ReactNode {
   const [inputValue, setInputValue] = useState(value);
   const [errorMessages, setErrorMessages] = useState([]);
+  const [copyAnimation, setCopyAnimation] = useState(false);
 
   const handleCopy = () => {
     navigator.clipboard.writeText(inputValue)
       .then(() => {
         console.log('Cube description copied to clipboard');
+        setCopyAnimation(true);
+        setTimeout(() => setCopyAnimation(false), 500); // Reset animation after 500ms
       })
       .catch((err) => {
         console.error('Failed to copy: ', err);
@@ -55,11 +58,15 @@ export default function DescriptionInput({value, onChange}) : ReactNode {
         />
         <button
           aria-label="Copy cube description"
-          className={clsx(drawingStyles["color-button"], drawingStyles["color-button--action"])}
+          className={clsx(
+            drawingStyles["color-button"],
+            drawingStyles["color-button--action"],
+            { [drawingStyles["color-button--copied"]]: copyAnimation }
+          )}
           onClick={handleCopy}
         >
           <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fillRule="evenodd" clipRule="evenodd" d="M19.5 16.5L19.5 4.5L18.75 3.75H9L8.25 4.5L8.25 7.5L5.25 7.5L4.5 8.25V20.25L5.25 21H15L15.75 20.25V17.25H18.75L19.5 16.5ZM15.75 15.75L15.75 8.25L15 7.5L9.75 7.5V5.25L18 5.25V15.75H15.75ZM6 9L14.25 9L14.25 19.5L6 19.5L6 9Z" />
+            <path fillRule="evenodd" clipRule="evenodd" d="M19.5 16.5L19.5 4.5L18.75 3.75H9L8.25 4.5L8.25 7.5L5.25 7.5L4.5 8.25V20.25L5.25 21H15L15.75 20.25V17.25H18.75L19.5 16.5ZM15.75 15.75L15.75 8.25L15 7.5L9.75 7.5V5.25L18 5.25V15.75H15.75ZM6 9L14.25 9L14.25 19.5L6 19.5L6 9Z" />
           </svg>
         </button>
       </div>
